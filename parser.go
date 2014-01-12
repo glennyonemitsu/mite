2720f9e38171fda900ba78408f8bbb7166a58207
Glenn Yonemitsu <yonemitsu@gmail.com>
2014-01-12 17:04:32 -0500

    node struct separated

--- parser.go
+++ parser.go
@@ -5,17 +5,6 @@ import (
 	"unicode/utf8"
 )
 
-type Node struct {
-	tag string
-	attrs map[string]string
-	text string
-
-	// dumb appended string while attribute assignment is still being determined
-	// this is used if it turns out tokens are NOT part of an attribute assignment
-	// and the string (with whitespace if any) gets used as the node's text
-	attrString string
-}
-
 type Parser struct {
 
 	debug bool
