b1e390d9645009392392e16f8aa2c83c291b61f7
Glenn Yonemitsu <yonemitsu@gmail.com>
2014-01-12 03:54:42 -0500

    much better parsing algo started, stack based

--- parser.go
+++ parser.go
@@ -5,53 +5,42 @@ import (
 	"unicode/utf8"
 )
 
-type Tag struct {
-	spaces int
+type Node struct {
 	tag string
+	attrs map[string]string
+	text string
 }
 
 type Parser struct {
 	Scanner Scanner
 	output string
 
-	// nested html tags for backtacking
-	//tags []string
-	// space counts for each indent level for backtacking
-	//indents []int
-
-	// heirarchy
-	line []Tag
-
-	// for indent calculations, how many spaces should a tab equal
-	tabSpace int
+	// node heirarchy
+	stack []*Node
 
 	// states as the Parser object figures out how to output the tokens
 
 	// new line has started
 	isNewLine bool
 
-	// rest of tokens before TokNewLine is regular text
-	isText bool
-
-	// whitespace is significant. This is to distinguish [] syntax, not indent
-	isBracket bool
-
 	isIndent bool
+	isDedent bool
+	dedentCount int
 
-	tag Tag
+	node *Node
 }
 
 func (p *Parser) Output() string {
 	var tokens []rune
 	var text string
 
-	p.line = []Tag{}
-	p.tabSpace = 8
+	p.stack = make([]*Node, 0)
 	p.output = ""
 	p.isNewLine = true
-	p.isText = false
-	p.isIndent = true
-	p.tag = Tag{spaces:0, tag:""}
+	p.isIndent = false
+	p.isDedent = false
+	p.dedentCount = 0
+	p.node = new(Node)
 
 	for {
 		tokens = p.Scanner.Scan()
@@ -67,43 +56,77 @@ func (p *Parser) Output() string {
 	return p.output
 }
 
-func (p *Parser) closeLastTag() {
-	if len(p.line) > 0 {
-		oldTag := p.line[len(p.line) - 1]
-		p.line = p.line[0: len(p.line) - 1]
-		if r, _ := utf8.DecodeLastRuneInString(p.output); r == ' ' {
-			p.output = p.output[0: len(p.output) - 1]
+func (p *Parser) closeLastNode() {
+	if len(p.stack) > 0 {
+		oldNode := p.stack[len(p.stack) - 1]
+		p.stack = p.stack[0: len(p.stack) - 1]
+		if r, l := utf8.DecodeLastRuneInString(p.output); r == ' ' {
+			p.output = p.output[0: len(p.output) - l]
 		}
-		p.output += "</" + oldTag.tag + ">"
+		p.output += "</" + oldNode.tag + ">"
 	}
+}
+
+func (p *Parser) lastNode() *Node {
+	return p.stack[len(p.stack) - 1]
+}
+
+func (p *Parser) outputNode(node *Node) {
+	p.output += "<" + node.tag + ">"
+	p.output += node.text
 }
 
 func (p *Parser) processToken(tok rune, text string) {
 	switch tok {
 	case TokWord:
-		if p.isNewLine {
-			// since there were no indents or dedents, this tag is the new end of the line
-			p.closeLastTag()
-			p.tag.tag = text
-			p.output += "<" + text + ">"
-			p.line = append(p.line, p.tag)
-			p.isNewLine = false
+		if p.node.tag == "" {
+			p.node.tag = text
 		} else {
-			p.output += text
-			p.output += " "
+			p.node.text += text
 		}
+	case TokWhitespace:
+		p.node.text += text
 	case TokIndent:
-		p.isNewLine = false
+		p.isIndent = true
+		p.isDedent = false
 	case TokDedent:
-		p.isNewLine = false
-		// for dedents, pop off the line and close the tag
-		p.closeLastTag()
+		p.isIndent = false
+		p.isDedent = true
+		p.dedentCount += 1
 	case TokNewLine:
-		p.isNewLine = true
+		// up to this point a node has been constructed but not appended to the
+		// output
+
+		fmt.Println(p.node)
+		if p.isDedent {
+			// for dedents, pop off the stack and close the node
+			for i := 0; i < p.dedentCount; i += 1 {
+				p.closeLastNode()
+			}
+			// closing one more time because this new node replaces the top node in the
+			// stack
+			p.closeLastNode()
+			p.outputNode(p.node)
+			p.stack = append(p.stack, p.node)
+		} else if p.isIndent {
+			p.outputNode(p.node)
+			p.stack = append(p.stack, p.node)
+		} else {
+			p.closeLastNode()
+			p.outputNode(p.node)
+			p.stack = append(p.stack, p.node)
+		}
+		p.node = new(Node)
+		p.isIndent = false
+		p.isDedent = false
 	case TokEOF:
-		// close the remaining tags in the line
-		for i := len(p.line) - 1; i >= 0; i -= 1 {
-			p.output += "</" + p.line[i].tag + ">"
+		if p.node.tag != "" {
+			p.stack = append(p.stack, p.node)
+			p.outputNode(p.node)
+		}
+		// close the remaining nodes in the stack 
+		for len(p.stack) > 0 {
+			p.closeLastNode()
 		}
 	}
 }
